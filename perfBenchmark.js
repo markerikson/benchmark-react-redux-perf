3173cae36b490bea7a8ee6e1025f0edb69ac16b3
Mark Erikson <mark@isquaredsoftware.com>
2018-08-07 22:56:33 -0400

    Rewrite perf runner to collect FPS stats for multiple versions

--- perfBenchmark.js
+++ perfBenchmark.js
@@ -3,16 +3,24 @@ const puppeteer = require("puppeteer");
 const tracealyzer = require('tracealyzer');
 const spawn = require("cross-spawn");
 
+const fs = require("fs");
+
 const TRACE_FILE = 'trace.json';
+
+const VERSIONS = ["5.0.7", "4.4.9"];
 
 const timeout = ms => new Promise(res => setTimeout(res, ms))
 
 
+/*
 const runBuildTask = spawn.sync('npm', ['run', 'build'], {
     stdio: 'inherit',
 });
 
 console.log("Build completed!");
+*/
+
+
 
 
 const app = express();
@@ -20,33 +28,43 @@ const app = express();
 app.use(express.static('build'))
 
 app.listen(9999, async () => {
-    console.log('Example app listening on port 3000!');
-
     const browser = await puppeteer.launch();
-    const page = await browser.newPage();
 
-    await page.evaluate(() => performance.setResourceTimingBufferSize(100000));
-    //await page.tracing.start({path: TRACE_FILE});
+    const versionPerfEntries = {};
 
+    for(let version of VERSIONS) {
+        const sourceFilename = `react-redux-${version}.min.js`;
+        fs.copyFileSync(sourceFilename, "build/react-redux.min.js");
 
-    await page.goto('http://localhost:9999');
+        const page = await browser.newPage();
+        await page.evaluate(() => performance.setResourceTimingBufferSize(100000));
 
-    await timeout(30000);
+        console.log(`Loading page for version ${version}...`)
+        await page.goto('http://localhost:9999');
 
-    //await page.tracing.stop();
+        await timeout(30000);
 
-    const perfEntries = JSON.parse(
-        await page.evaluate(() => JSON.stringify(performance.getEntries()))
-    );
+        //await page.tracing.stop();
 
-    const fpsStatsEntries = JSON.parse(
-        await page.evaluate(() => JSON.stringify(window.getFpsStats()))
-    );
+        const perfEntries = JSON.parse(
+            await page.evaluate(() => JSON.stringify(performance.getEntries()))
+        );
+
+        const fpsStatsEntries = JSON.parse(
+            await page.evaluate(() => JSON.stringify(window.getFpsStats()))
+        );
+
+        const fpsValues = fpsStatsEntries.map(entry => entry.meta.details.FPS);
+
+        versionPerfEntries[version] = fpsValues;
+
+        await page.close();
+    }
 
     await browser.close();
 
-    const fpsValues = fpsStatsEntries.map(entry => entry.meta.details.FPS);
-    console.log(fpsValues);
+
+    console.log(versionPerfEntries);
 
     //const metrics = tracealyzer(TRACE_FILE);
 
