531df76d636ddf3503d7c9c915108d7e369711e1
Mark Erikson <mark@isquaredsoftware.com>
2018-08-07 21:53:45 -0400

    Copy-paste FPS capture setup

--- src/index.js
+++ src/index.js
@@ -5,7 +5,40 @@ import App from './App';
 
 import {Provider} from "react-redux";
 
+import {PerformanceMetadataMarker} from "performance-mark-metadata";
+import FpsEmitter from "fps-emitter";
+//const { PerformanceMetadataMarker } = require("performance-mark-metadata");
+//const FpsEmitter = require("fps-emitter");
+
 import configureStore from "./configureStore";
+
+
+const marker = new PerformanceMetadataMarker();
+window.marker = marker;
+
+const fps = new FpsEmitter();
+fps.on("update", function(FPS) {
+    // mark current FPS
+    marker.mark("FPS", {
+        details: { FPS }
+    });
+});
+
+const getFpsStats = () => {
+    const logData = performance.getEntriesByType("mark").map(entry => {
+        const meta = marker.getEntryMetadata(entry);
+        return {
+            type: entry.name,
+            timeStamp: entry.startTime,
+            meta: meta
+        };
+    });
+
+    return logData;
+}
+
+window.getFpsStats = getFpsStats;
+
 
 const store = configureStore();
 
